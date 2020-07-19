7202da9e499e8da962882a82439fa506aae89ab5
gondi, jagapathi <jagapathi.gondi@cerner.com>
2020-07-19 17:19:42 +0530

    new site added

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 YUI().use(
     'aui-tree-view',
     function(Y) {
-      var children = [
-        {
-          children: [
-            {
-              label: 'File X'
-            },
-            {
-              label: 'File Y'
-            },
-            {
-              label: 'File Z'
-            }
-          ],
-          expanded: true,
-          label: 'Root'
-        }
-      ];
-  
-      new Y.TreeView(
-        {
-          boundingBox: '#myTreeView',
-          children: children
-        }
-      ).render();
+        var children = [{
+            children: [{
+                    label: 'Profile'
+                },
+                {
+                    label: 'Experience'
+                },
+                {
+                    label: 'Education'
+                },
+                {
+                    label: 'Skills'
+                },
+                {
+                    label: 'Achievments'
+                },
+                {
+                    label: 'Projects'
+                },
+                {
+                    label: 'Contact'
+                }
+            ],
+            expanded: true,
+            label: 'Jagapathi Gondi'
+        }];
+
+        new Y.TreeView({
+            boundingBox: '#myTreeView',
+            children: children
+        }).render();
     }
-  );
+);
